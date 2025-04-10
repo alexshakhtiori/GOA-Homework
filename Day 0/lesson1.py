c6fac4a632507b5f7b485e87ca753c458fae7e3a
alexshakhtiori <alexbuadze22@gmail.com>
2025-04-11 00:44:30 +0400

    first commit

---
+++ Day 0/lesson1.py
@@ -0,0 +1,73 @@
+from turtle import *
+
+#we want to build a house
+
+#step 1: square 
+speed(30)
+shape("turtle")
+width(7)
+color("pink")
+forward(200)     #house height
+left(90)
+forward(200)      #house length
+left(90)
+forward(200)
+left(90)
+forward(200)
+
+
+#onto the door
+left(90)
+forward(50)
+color("black")
+begin_fill()
+left(90)
+forward(70)   #door height
+right(90)
+forward(25)     #door width
+right(90)
+forward(70)
+end_fill()
+penup()
+goto(200, 200)
+pendown()
+
+#roof
+
+color("green")
+begin_fill()
+right(150)
+forward(200)     #roof height
+left(120)
+forward(200)
+end_fill()
+
+#window
+color("blue")
+penup()
+goto(150, 70)
+pendown()
+left(120)
+forward(10)
+left(90)
+forward(50)     #window height
+left(90)
+forward(40)     #window length
+left(90)
+forward(50)
+left(90)
+forward(40)
+left(180)
+forward(20)
+right(90)
+forward(50)
+left(180)
+forward(25)
+right(90)
+forward(20)
+right(180)
+forward(40)
+
+exitonclick()
+
+print("GOA <3")
